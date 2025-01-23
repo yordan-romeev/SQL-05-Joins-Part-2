52fbc0259b4ac0b518c0f7a39f6dcf1a23f38903
Yordan Romeev <yordan.romeev@gmail.com>
2025-01-23 11:28:48 +0200

    Add Task 2

--- 02. SQL Joins - Additional SirmaAcademyJoinsDB Queries.sql
+++ 02. SQL Joins - Additional SirmaAcademyJoinsDB Queries.sql
@@ -9,6 +9,17 @@ FROM Orders o
     JOIN Customers c ON c.CustomerID = o.CustomerID
     JOIN Employees e ON e.EmployeeID = o.EmployeeID
     JOIN Shippers s ON s.ShipperID = o.ShipperID
-    JOIN (SELECT od.OrderID, SUM(od.Quantity * p.Price) as Total
+    JOIN (  SELECT od.OrderID, SUM(od.Quantity * p.Price) as Total
     FROM OrderDetails od JOIN Products p ON od.ProductID = p.ProductID
     GROUP By od.OrderID) AS TotalPrice ON TotalPrice.OrderID = o.OrderID
+
+-- ## 2: Products Supplied by Each Supplier for Specific Customers
+-- Write a query to display the `SupplierName`, `ProductName`, `CustomerName`, and the total quantity ordered by the customer. Use the `Orders`, `OrderDetails`, `Products`, `Suppliers`, and `Customers` tables. Group the results by `SupplierName`, `ProductName`, and `CustomerName`.
+
+SELECT s.SupplierName, p.ProductName, c.CustomerName, SUM(od.Quantity) as TotalQuantity
+FROM Orders o
+    JOIN OrderDetails od ON od.OrderID = o.OrderID
+    JOIN Products p ON p.ProductID = od.ProductID
+    JOIN Suppliers s ON s.SupplierID = p.SupplierID
+    JOIN Customers c ON c.CustomerID = o.CustomerID
+GROUP BY s.SupplierName, p.ProductName, c.CustomerName
